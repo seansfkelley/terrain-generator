945c27e1f283c06b0e2573f1dc156cbe876bce8a
Sean Kelley <seansfkelley@gmail.com>
2017-10-13 18:45:56 -0700

    Mash in more example code so something finally works.
    Taken from https://gist.github.com/simias/c140d1479ada4d6218c0, which uses
    SDL, so I pulled out the necessary parts and kept using GLFW.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+#![feature(libc)]
+
 extern crate glfw;
 extern crate gl;
+extern crate libc;
 
 use std::mem;
 use std::ptr;
+use std::str;
+use std::ffi::CString;
 use glfw::Context;
 use gl::types::*;
 
@@ -24,38 +29,52 @@ fn main() {
 
     gl::load_with(|s| window.get_proc_address(s) as *const _);
 
-    let g_vertex_buffer_data: [GLfloat; 9] = [
-        -1.0, -1.0, 0.0,
-         1.0, -1.0, 0.0,
-         0.0,  1.0, 0.0,
-    ];
+    let vs = compile_shader(VS_SRC, gl::VERTEX_SHADER);
+    let fs = compile_shader(FS_SRC, gl::FRAGMENT_SHADER);
+
+    let program = link_program(vs, fs);
+
+    let mut vao = 0;
+    let mut vbo = 0;
+
+    unsafe {
+        // Create Vertex Array Object
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+
+        // Create a Vertex Buffer Object and copy the vertex data to it
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(gl::ARRAY_BUFFER,
+                       (VERTEX_DATA.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                       mem::transmute(&VERTEX_DATA[0]),
+                       gl::STATIC_DRAW);
+
+        // Use shader program
+        gl::UseProgram(program);
+        gl::BindFragDataLocation(program, 0,
+                                 CString::new("out_color").unwrap().into_raw());
+
+        // Specify the layout of the vertex data
+        let pos_attr = gl::GetAttribLocation(program,
+                                             CString::new("position").unwrap().into_raw());
+        gl::EnableVertexAttribArray(pos_attr as GLuint);
+        gl::VertexAttribPointer(pos_attr as GLuint, 2, gl::FLOAT,
+                                gl::FALSE as GLboolean, 0, ptr::null());
+    }
 
     while !window.should_close() {
         unsafe {
-            let mut vertex_array_id: GLuint = 0;
-            gl::GenVertexArrays(1, &mut vertex_array_id);
-            gl::BindVertexArray(vertex_array_id);
+            // Clear the screen to black
+            gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
 
-            let mut vertex_buffer: GLuint = 0;
-            gl::GenBuffers(1, &mut vertex_buffer);
-            gl::BindBuffer(gl::ARRAY_BUFFER, vertex_buffer);
-            gl::BufferData(gl::ARRAY_BUFFER, (g_vertex_buffer_data.len() * mem::size_of::<GLfloat>()) as GLsizeiptr, mem::transmute(&g_vertex_buffer_data[0]), gl::STATIC_DRAW);
-
-            gl::EnableVertexAttribArray(0);
-            gl::BindBuffer(gl::ARRAY_BUFFER, vertex_buffer);
-            gl::VertexAttribPointer(
-                0,
-                3,
-                gl::FLOAT,
-                gl::FALSE,
-                0,
-                ptr::null()
-            );
+            // Draw a triangle from the 3 vertices
             gl::DrawArrays(gl::TRIANGLES, 0, 3);
-            gl::DisableVertexAttribArray(0);
+
+            window.swap_buffers();
         }
 
-        window.swap_buffers();
         glfw.poll_events();
 
         for (_, event) in glfw::flush_messages(&events) {
@@ -70,5 +89,76 @@ fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
             window.set_should_close(true)
         }
         _ => {}
+    }
+}
+
+// Vertex data
+static VERTEX_DATA: [GLfloat; 6] = [
+    0.0,  0.5,
+    0.5, -0.5,
+    -0.5, -0.5
+];
+
+// Shader sources
+static VS_SRC: &'static str =
+    "#version 150\n\
+    in vec2 position;\n\
+    void main() {\n\
+        gl_Position = vec4(position, 0.0, 1.0);\n\
+    }";
+
+static FS_SRC: &'static str =
+   "#version 150\n\
+    out vec4 out_color;\n\
+    void main() {\n\
+       out_color = vec4(1.0, 1.0, 1.0, 1.0);\n\
+    }";
+
+fn compile_shader(src: &str, ty: GLenum) -> GLuint {
+    let shader;
+    unsafe {
+        shader = gl::CreateShader(ty);
+        // Attempt to compile the shader
+        let c_str = CString::new(src.as_bytes()).unwrap().into_raw();
+        gl::ShaderSource(shader, 1, mem::transmute(&c_str), ptr::null());
+        gl::CompileShader(shader);
+
+        // Get the compile status
+        let mut status = gl::FALSE as GLint;
+        gl::GetShaderiv(shader, gl::COMPILE_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len = 0;
+            gl::GetShaderiv(shader, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::new();
+            buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+            gl::GetShaderInfoLog(shader, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+            panic!("{}", str::from_utf8(buf.as_slice()).ok().expect("ShaderInfoLog not valid utf8"));
+        }
+    }
+    shader
+}
+
+fn link_program(vs: GLuint, fs: GLuint) -> GLuint {
+    unsafe {
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, vs);
+        gl::AttachShader(program, fs);
+        gl::LinkProgram(program);
+        // Get the link status
+        let mut status = gl::FALSE as GLint;
+        gl::GetProgramiv(program, gl::LINK_STATUS, &mut status);
+
+        // Fail on error
+        if status != (gl::TRUE as GLint) {
+            let mut len: GLint = 0;
+            gl::GetProgramiv(program, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf = Vec::new();
+            buf.set_len((len as usize) - 1); // subtract 1 to skip the trailing null character
+            gl::GetProgramInfoLog(program, len, ptr::null_mut(), buf.as_mut_ptr() as *mut GLchar);
+            panic!("{}", str::from_utf8(buf.as_slice()).ok().expect("ProgramInfoLog not valid utf8"));
+        }
+        program
     }
 }
