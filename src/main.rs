4997f33f73c944a9f2611bfaee22679419608280
Sean Kelley <seansfkelley@gmail.com>
2017-10-14 13:18:52 -0700

    Try to add debug hooks to figure out why it doesn't work, but Apple says no.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@ mod shaders;
 
 use std::mem;
 use std::ptr;
-use std::ffi::CString;
+use std::os::raw::{ c_void, c_char };
+use std::ffi::{ CString, CStr };
 use num_traits::identities::One;
 use glfw::Context;
 use gl::types::*;
@@ -28,8 +29,28 @@ static VERTEX_DATA: [GLfloat; 6] = [
 static WIDTH: u32 = 400;
 static HEIGHT: u32 = 300;
 
-fn to_c_str(s: &str) -> *mut std::os::raw::c_char {
+fn to_c_str(s: &str) -> *mut c_char {
     return CString::new(s).unwrap().into_raw();
+}
+
+extern "system" fn gl_debug_message(
+    source: GLenum,
+    type_: GLenum,
+    id: GLuint,
+    severity: GLenum,
+    _length: GLsizei,
+    message: *const GLchar,
+    _user_param: *mut c_void
+) {
+    unsafe {
+        debug!(
+            "OpenGL [{}]: (source: {}, type: {}, id: {}) {}",
+            severity,
+            source,
+            type_,
+            id,
+            CStr::from_ptr(message).to_str().unwrap());
+    }
 }
 
 fn main() {
@@ -40,10 +61,11 @@ fn main() {
     info!("successfully initialized GLFW");
 
     glfw.window_hint(glfw::WindowHint::Samples(Option::Some(4)));
-    glfw.window_hint(glfw::WindowHint::ContextVersionMajor(3));
-    glfw.window_hint(glfw::WindowHint::ContextVersionMinor(3));
+    glfw.window_hint(glfw::WindowHint::ContextVersionMajor(4));
+    glfw.window_hint(glfw::WindowHint::ContextVersionMinor(0));
     glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    glfw.window_hint(glfw::WindowHint::OpenGlDebugContext(true));
 
     let (mut window, events) = glfw
         .create_window(WIDTH, HEIGHT, "terrain-generator", glfw::WindowMode::Windowed)
@@ -55,6 +77,27 @@ fn main() {
     window.make_current();
 
     gl::load_with(|s| window.get_proc_address(s) as *const _);
+
+    unsafe {
+        info!("OpenGL version {}, GLSL version {}",
+            CStr::from_ptr(gl::GetString(gl::VERSION) as *const c_char).to_string_lossy(),
+            CStr::from_ptr(gl::GetString(gl::SHADING_LANGUAGE_VERSION) as *const c_char).to_string_lossy());
+
+        let mut major_version = -1;
+        let mut minor_version = -1;
+        gl::GetIntegerv(gl::MAJOR_VERSION, &mut major_version);
+        gl::GetIntegerv(gl::MINOR_VERSION, &mut minor_version);
+        if major_version > 4 || (major_version == 4 && minor_version >= 3) {
+            gl::Enable(gl::DEBUG_OUTPUT);
+            gl::Enable(gl::DEBUG_OUTPUT_SYNCHRONOUS);
+            gl::DebugMessageCallback(gl_debug_message, ptr::null());
+            gl::DebugMessageControl(gl::DONT_CARE, gl::DONT_CARE, gl::DONT_CARE, 0, ptr::null(), gl::TRUE);
+            info!("enabled OpenGL logging at 'debug' level");
+        } else {
+            // Fucking OS X is using a 7-year-old OpenGL version that doesn't support this.
+            warn!("OpenGL version is too old; will not enable OpenGL debug logging");
+        }
+    }
 
     let vs = shaders::compile_shader("./shaders/basic.vert", gl::VERTEX_SHADER);
     let fs = shaders::compile_shader("./shaders/white.frag", gl::FRAGMENT_SHADER);
@@ -74,12 +117,21 @@ fn main() {
     );
     let model = glm::Mat4::one();
 
-    let mvp = projection * view * model;
+    let mvp = model;
+    let mut mvp_array = Vec::new();
+
+    for i in 0..4 {
+        for j in 0..4 {
+            mvp_array.push(mvp[i][j]);
+        }
+    }
+
+    println!("{:?}", mvp_array);
 
     unsafe {
         // MVP
         let matrix_id = gl::GetUniformLocation(program, to_c_str("mvp"));
-        gl::UniformMatrix4fv(matrix_id, 1, gl::FALSE, &mvp[0][0]);
+        gl::UniformMatrix4fv(matrix_id, 1, gl::FALSE, mvp_array.as_ptr());
 
         // VAO
         gl::GenVertexArrays(1, &mut vao);
